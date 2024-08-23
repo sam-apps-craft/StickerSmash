7aa8fd3c86a72f2a9525912332399589976a027f
Samantha Raymond <>
2024-08-23 15:27:01 +0100

    buttons added

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import ImageViewer from './components/ImageViewer';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
 
-const PlaceholderImage = require('./assets/images/background-image.png');
-
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage}/>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
-      <Text style={{ color: '#fff' }}>Open up App.js to start working on your app!</Text>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -21,12 +25,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
   imageContainer: {
     flex: 1,
     paddingTop: 58,
   },
-
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
 });
